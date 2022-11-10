417facf1efabd8126e2b2e6a84ab47af0e9dc290
hbdevops1 <hugob@outlook.com>
2022-11-10 14:06:46 -0500

    adding master

--- N1.main.tf
+++ N1.main.tf
@@ -5,13 +5,13 @@
   
 
 resource "azurerm_resource_group" "rg-wkspc" {
-  count = 1
+  /* count = 1 */
    provisioner "local-exec" {
     command = "az group list --query \"{ResourceGroup: '${local.naming_convention.name}'}\""
    
   }
   
-  name     = (var.name == "local.naming_convention.name") ? local.naming_convention.name :  trimprefix(format("%0s%03d", local.naming_convention.name, 2), local.naming_convention.name)
+  name     = (var.name == "local.naming_convention.name") ? local.naming_convention.name :  trimprefix(format("%0s%03d", local.naming_convention.name, 1), local.naming_convention.name)
   location = var.resource_group_location
 
   
